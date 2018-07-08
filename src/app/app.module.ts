bb4b6c3bf463537f481f22dc224108d5b92986c4
Alexey Miroshnychenko <muzz0neasdqwe@gmail.com>
2018-07-08 10:12:42 +0300

    routinng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material';
+import {AuthModule} from './auth/auth.module';
+import {RouterModule} from '@angular/router';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    AuthModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
